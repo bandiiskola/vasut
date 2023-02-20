d3fbfd76685854779cf4f2466a8cdb6f573e150c
bandiiskola <nandor.bere@gmail.com>
2023-02-20 14:29:21 +0100

    mukszik

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
 const allomasok=['Nyugati','Zugló','Kőbánya-Kispest','Ferihegy','Vecsés','Üllő','Monor','Monorierdő','Pilis','Albertirsa','Ceglédbercel','Cegléd','Abony','Szolnok','Szajol','Törökszentmiklós','Fegyvernek-Örményes','Kisújszállás'];
 
 
-function ElotteUtana(megallo)
-{
+function ElotteUtana(megallo){
     var p=allomasok.indexOf(megallo);
     var r="";
     if  (p==-1){
@@ -23,4 +22,9 @@ function ElotteUtana(megallo)
         }
     }
     return r;
+}
+
+function Keres(){
+    var anev=document.getElementById("allomasnev").value;
+    document.getElementById("eredmeny").innerHTML=ElotteUtana(anev);
 }
