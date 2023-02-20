c1b791d01304ef9756b8c0574177360dcd9e31c3
bandiiskola <nandor.bere@gmail.com>
2023-02-20 14:14:40 +0100

    Megírtam az ElotteUtana függvényt

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const allomasok=['Nyugati','Zugló','Kőbánya-Kispest','Ferihegy','Vecsés','Üllő','Monor','Monorierdő','Pilis','Albertirsa','Ceglédbercel','Cegléd','Abony','Szolnok','Szajol','Törökszentmiklós','Fegyvernek-Örményes','Kisújszállás'];
+
+
+function ElotteUtana(megallo)
+{
+    var p=allomasok.indexOf(megallo);
+    var r="";
+    if  (p==-1){
+        r="Nincs ilyen megálló ezen a vonalon"
+    }
+    else{
+        if(p!=0){
+            r+="Előtte: "+allomasok[p-1];
+            
+        }
+
+        r+="<br>"
+        if(p==allomasok.length-1){
+            r+="Utána: -";
+        }
+        else {
+            r+="Utána: "+allomasok[p+1];
+        }
+    }
+    return r;
+}
